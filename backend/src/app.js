f523feded7aeb494da32fa3a28d7c25831273760
Jackson <jackson.marques1@outlook.com>
2026-05-30 21:12:44 -0400

    Adicionar criação de agendamentos

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,8 @@ const appointmentRoutes = require("./routes/appointment.routes");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json({
     message: "Bem-vindo à API do Gerenciador de Agendamentos da Barbearia!",
--- backend/src/routes/appointment.routes.js
+++ backend/src/routes/appointment.routes.js
@@ -14,4 +14,37 @@ router.get("/appointments", async (req, res) => {
     });
   }
 });
+
+router.post("/appointments", async (req, res) => {
+  try {
+    const { client_name, barber_name, appointment_date, appointment_time } =
+      req.body;
+
+    if (
+      !client_name ||
+      !barber_name ||
+      !appointment_date ||
+      !appointment_time
+    ) {
+      return res.status(400).json({
+        message: "Preencha todos os campos obrigatórios.",
+      });
+    }
+
+    const result = await pool.query(
+      `INSERT INTO appointments 
+      (client_name, barber_name, appointment_date, appointment_time)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *`,
+      [client_name, barber_name, appointment_date, appointment_time],
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({
+      message: "Erro ao criar agendamento.",
+    });
+  }
+});
+
 module.exports = router;
